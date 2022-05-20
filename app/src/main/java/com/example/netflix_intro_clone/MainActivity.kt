bfa358b584a1b91d74afe9604272b00c6e984602
tjdrms <tjdrms0116>
2022-05-20 15:47:21 +0900

    넷플릭스 인트로 애니메이션 클로닝

--- app/src/main/java/com/example/netflix_intro_clone/MainActivity.kt
+++ app/src/main/java/com/example/netflix_intro_clone/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.netflix_intro_clone
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.netflix_intro_clone.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +25,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        makeStatusBarTransparent()
+
+        initAppBar()
+
+        initInsetMargin()
 
         binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
             if (binding.scrollView.scrollY > 150f.dpToPx(this).toInt()) {
@@ -60,6 +75,57 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun Float.dpToPx(context: Context): Float =
-        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+    private fun initInsetMargin() = with(binding) {
+        ViewCompat.setOnApplyWindowInsetsListener(coordinator) {v: View, insets: WindowInsetsCompat ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = insets.systemWindowInsetBottom
+            toolBarContainer.layoutParams = (toolBarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0, insets.systemWindowInsetTop, 0, 0)
+            }
+            collapsingToolBarContainer.layoutParams = (collapsingToolBarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0,0,0,0)
+            }
+
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun initAppBar() {
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val topPadding = 120f.dpToPx(this)
+            val abstractOffSet = abs(verticalOffset)
+            if (abstractOffSet < topPadding) {
+                binding.toolBarBackgroundView.alpha = 0f
+                return@OnOffsetChangedListener
+            }
+            val verticalOffsetByTopPadding = abstractOffSet - topPadding
+            val percentage = abs(verticalOffsetByTopPadding) / appBarLayout.totalScrollRange
+            binding.toolBarBackgroundView.alpha = 1 - (if(1 - percentage * 2 < 0) 0f else 1 - percentage * 2)
+        })
+        initActionBar()
+    }
+
+    private fun initActionBar() = with(binding) {
+        toolBar.navigationIcon = null
+        toolBar.setContentInsetsAbsolute(0, 0)
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+            it.setDisplayShowHomeEnabled(false)
+        }
+    }
+
+}
+
+fun Float.dpToPx(context: Context): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+
+fun Activity.makeStatusBarTransparent() {
+    window.apply {
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
+    }
 }
